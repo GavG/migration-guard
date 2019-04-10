854740b60e960f24850b14591a4ec4789a28a4a8
GavG <gavingration@gmail.com>
2019-04-10 21:30:21 +0100

    rename files for auto stuff...

--- src/database/migrations/add_hash_column_to_migration.php
+++ src/database/migrations/add_hash_column_to_migration.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddHashColumnToMigrationsTable extends Migration
+class AddHashColumnToMigration extends Migration
 {
 
     public function up()
