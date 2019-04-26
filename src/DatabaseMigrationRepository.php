ecb5f326c68ca3bdfc73281ef7511d9209f3ef3c
rs-sliske <rs-sliske@users.noreply.github.com>
2019-04-26 16:22:57 +0100

    Update DatabaseMigrationRepository.php

--- src/DatabaseMigrationRepository.php
+++ src/DatabaseMigrationRepository.php
@@ -6,23 +6,39 @@ use Illuminate\Database\Migrations\DatabaseMigrationRepository as DefaultDatabas
 
 class DatabaseMigrationRepository extends DefaultDatabaseMigrationRepository
 {
+    public function log($file, $batch, $hash = null)
+    {
+        $record = $this->haveHashColumn() ? ['migration' => $file, 'batch' => $batch, 'hash' => $hash] : ['migration' => $file, 'batch' => $batch];
+        $this->table()->insert($record);
+    }
 
-  public function log($file, $batch, $hash = null)
-  {
-     $record = $hash ? ['migration' => $file, 'batch' => $batch] : ['migration' => $file, 'batch' => $batch, 'hash' => $hash];
-     $this->table()->insert($record);
-  }
-  
-  public function getRows()
-  {
-    return $this->table()
+    public function getRows()
+    {
+        return $this->table()
                ->orderBy('batch', 'asc')
                ->orderBy('migration', 'asc')
                ->get();
-  }
-  
-  public function update($id, $data)
-  {
-    $this->table()->where('id', $id)->update($data);
-  }
+    }
+
+    public function update($id, $data)
+    {
+        $this->table()->where('id', $id)->update($data);
+    }
+
+    private function haveHashColumn()
+    {
+        static $res;
+
+        if ($res) {
+            return true;
+        }
+
+        $migration = $this->getRows()[0] ?? [];
+
+        if (is_null($migration)) {
+            return false;
+        }
+
+        return $res = array_key_exists('hash', $migration);
+    }
 }
